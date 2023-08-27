08795d1ea2cda240849f4efa4f3d0a86bb00b184
Emmalcd <joselemmanuel9@gmail.com>
2023-08-27 10:45:20 -0600

    Ejercicios de arreglos

---
+++ js/arreglos.js
@@ -0,0 +1,14 @@
+const a = [];
+const b = [5, true, "Hola", ["A","B","C"]];
+
+console.log(a);
+console.log("------------------------------------");
+console.log(b);
+console.log(b[1]);
+console.log(b[3][2]);
+
+const c = Array.of(1,2,3,4,5);
+
+console.log(c);
+
+console.log(c.length); //devuelve 5
