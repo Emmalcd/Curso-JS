614f62c1eccf2dc53e872e9648f2dfd076bc5ef0
Emmalcd <joselemmanuel9@gmail.com>
2023-08-29 11:23:52 -0600

    Ejercicio con break y continue

---
+++ js/break_continue.js
@@ -0,0 +1,15 @@
+for(let i=0; i<11;i++){
+    if(i==5){
+        break;
+    }
+    console.log(i);
+}
+
+console.log("******************");
+
+for(let i=0; i<11;i++){
+    if(i%2!=0){
+        continue;
+    }
+    console.log(i);
+}
