cc87e91beff5e9968c49fba956152bd4e31d028b
Emmalcd <joselemmanuel9@gmail.com>
2023-08-30 09:14:27 -0600

    Ejercicio de objetos literales

---
+++ js/objetos_literales.js
@@ -0,0 +1,21 @@
+let nombre = 'Homero';
+let edad = 7;
+
+let perro = {
+    nommbre: nombre,
+    edad: edad,
+    ladrar: function(){
+        console.log("El perro esta ladrando");
+    }
+}
+
+perro.ladrar();
+
+let dog ={
+    nombre, 
+    edad,
+    morder(){
+        console.log("El perro esta mordiendo");
+    }
+}
+dog.morder();
