f944fa249bf918374b9e4b8cdd7c63cd5ecf941f
Emmalcd <joselemmanuel9@gmail.com>
2023-08-31 13:01:15 -0600

    Agregando ejercicio de prototipos y funciones constructoras

---
+++ js/prototipos.js
@@ -0,0 +1,14 @@
+function Persona(nombre, edad){
+    this.nombre=nombre;
+    this.edad=edad;
+}
+
+Persona.prototype.Saludar=function(){
+    console.log(`Hola ${this.nombre}`);
+}
+
+
+let Emma = new Persona("Emmanuel", 23);
+console.log(Emma);
+
+Emma.Saludar();
