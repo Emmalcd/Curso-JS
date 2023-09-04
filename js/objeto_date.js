ad6356b74497d247896fae507de9fbb84dfc6c11
Emmalcd <joselemmanuel9@gmail.com>
2023-09-04 16:06:55 -0600

    Ejercicios con el objeto Date

---
+++ js/objeto_date.js
@@ -0,0 +1,10 @@
+console.log(Date);
+
+let fecha = new Date();
+
+console.log(fecha.getDate());
+
+let fecha2= new Date(5,1,4);
+
+console.log(fecha2.toDateString());
+console.log(fecha2.toString());
